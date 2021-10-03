7016ec7b51deaa8039a12f3743763f776294f2dc
Jeremy <peace@trippyogi.com>
2021-10-02 22:01:08 -0700

    Add max quantity test

--- test/metatravelers-test.js
+++ test/metatravelers-test.js
@@ -1,7 +1,10 @@
 const { expectRevert } = require('@openzeppelin/test-helpers');
 const { ethers } = require('hardhat');
 
-let metaTravelers;
+const MAX_SUPPLY = 7777;
+const MAX_QUANTITY = 10;
+const PRICE = 0.1;
+let metaTravelers, owner, address1, address2;
 
 describe('MetaTravelers', function () {
   this.beforeEach(async () => {
@@ -12,12 +15,25 @@ describe('MetaTravelers', function () {
       'baseTokenUri'
     );
     await metaTravelers.deployed();
+    [owner, address1, address2] = await ethers.getSigners();
   });
 
   it('Should revert if the provided tokenId does not exist', async function () {
     await expectRevert(
       metaTravelers.tokenURI(0),
       'ERC721Metadata: URI query for nonexistent token.'
+    );
+  });
+
+  it('should revert if quantity exceeds max quantity', async () => {
+    const price = PRICE;
+    const quantity = MAX_QUANTITY + 1;
+    const total = price * quantity;
+    await expectRevert(
+      metaTravelers.mint(address1.address, quantity, {
+        value: ethers.utils.parseEther(total.toString())
+      }),
+      'Order exceeds max quantity'
     );
   });
 });
