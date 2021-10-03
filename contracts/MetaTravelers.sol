6118a97bf95fb1962a028e457a1e1e673d348b55
Jeremy <peace@trippyogi.com>
2021-10-02 21:26:52 -0700

    Configure unit testing

--- contracts/MetaTravelers.sol
+++ contracts/MetaTravelers.sol
@@ -67,7 +67,7 @@ contract MetaTravelers is ERC721Enumerable, ERC721Pausable, ERC721Burnable, Owna
      * @dev See {IERC721Metadata-tokenURI}.
      */
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token.");
         return bytes(_baseTokenURI).length > 0 ? string(abi.encodePacked(_baseTokenURI, tokenId.toString())) : "";
     }   
 
---
+++ test/metatravelers-test.js
@@ -0,0 +1,25 @@
+const {
+  expectRevert // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+
+const { ethers } = require('hardhat');
+let metaTravelers;
+
+describe('MetaTravelers', function () {
+  this.beforeEach(async () => {
+    const MetaTravelers = await hre.ethers.getContractFactory('MetaTravelers');
+    metaTravelers = await MetaTravelers.deploy(
+      'MetaTravelers',
+      'MT',
+      'baseTokenUri'
+    );
+    await metaTravelers.deployed();
+  });
+
+  it('Should revert if the provided tokenId does not exist', async function () {
+    await expectRevert(
+      metaTravelers.tokenURI(0),
+      'ERC721Metadata: URI query for nonexistent token.'
+    );
+  });
+});
