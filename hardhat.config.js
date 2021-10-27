a4b76b3132c48072c3cc0be5f78e1b3540e43967
mikejohnston <mike.david.johnston@gmail.com>
2021-10-27 07:32:42 -0600

    remove unneeded code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,63 +1,50 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-web3');
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-web3')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: '0.4.24'
-      },
-      {
-        version: '0.6.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 0
-          }
-        }
-      },
-      {
-        version: '0.6.12',
-        settings: {
-          optimizer: {
-            enabled: true
-          }
-        }
-      },
-      {
-        version: '0.7.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 0
-          }
-        }
-      },
-      {
-        version: '0.8.7',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 0
-          }
-        }
-      }
-    ]
-  }
-};
+	solidity: {
+		compilers: [
+			{
+				version: '0.4.24',
+			},
+			{
+				version: '0.6.6',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+			{
+				version: '0.6.12',
+				settings: {
+					optimizer: {
+						enabled: true,
+					},
+				},
+			},
+			{
+				version: '0.7.6',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+			{
+				version: '0.8.7',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+		],
+	},
+}
