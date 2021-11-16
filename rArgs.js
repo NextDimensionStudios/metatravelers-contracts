8b2f8795c27b62cd763309ffd2edff276a718109
mikejohnston <mike.david.johnston@gmail.com>
2021-11-16 14:00:05 -0700

    add arguments for rinkeby verify

---
+++ rArgs.js
@@ -0,0 +1,11 @@
+// Rinkeby arguments for etherscan verify
+module.exports = [
+	'MetaTravelers',
+	'MT',
+	'uri',
+	// Rinkeby LINK
+	'0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B',
+	'0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
+	'0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311',
+	'100000000000000000',
+]
